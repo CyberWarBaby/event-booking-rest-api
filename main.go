888aa323add2350175bd1f858047cb7f5afba787
cyberwarbaby <habeebjimoh29@gmail.com>
2025-11-09 21:25:21 +0000

    http request handling

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"example.com/eventbookingrestapi/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,9 +13,30 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents) // GET, POST, PUT, DELETE, PATCH
-	server.Run(":8080")              //localhost:8080
+	server.POST("/events", createEvent)
+
+	server.Run(":8080") //localhost:8080
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"messages": "hello!"})
+	events := models.GetAllEvents()
+	// context.JSON(http.StatusOK, gin.H{"messages": "hello!"})
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse req data"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"message": "successfully created event", "event": event})
 }

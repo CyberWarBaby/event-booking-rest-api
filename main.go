e813bb9fcb8f217a0648a4c79ce4647fecf35061
cyberwarbaby <habeebjimoh29@gmail.com>
2025-11-10 12:15:31 +0000

    Started working on database connection

--- main.go
+++ main.go
@@ -21,7 +21,10 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events. Try again later"})
+	}
 	// context.JSON(http.StatusOK, gin.H{"messages": "hello!"})
 	context.JSON(http.StatusOK, events)
 }
@@ -38,7 +41,10 @@ func createEvent(context *gin.Context) {
 	event.ID = 1
 	event.UserID = 1
 
-	event.Save()
+	err = event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create events. Try again later"})
+	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "successfully created event", "event": event})
 }
